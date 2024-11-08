da183f3c8a03d97ef28b7d24628b403f338b778a
PRIYASAKI <125540653+PRIYASAKI@users.noreply.github.com>
2024-11-09 03:05:36 +0530

    fourth commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Add this line to set a relative base path
   build: {
-    target: 'esnext', // Ensure Vite outputs modern JavaScript modules
+    target: 'esnext',
     modulePreload: true,
-  }
-})
+  },
+});
