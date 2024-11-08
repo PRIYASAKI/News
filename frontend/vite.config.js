fc20ee85c3fe3190e721ebad32cd0199c6d129a8
PRIYASAKI <125540653+PRIYASAKI@users.noreply.github.com>
2024-11-09 03:20:24 +0530

    sixth commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Set a relative base path
+  base: '/', // Use absolute path to avoid issues
   build: {
     target: 'esnext',
     modulePreload: true,
-    assetsDir: './assets', // Ensure assets are handled correctly in Netlify
+    assetsDir: 'assets', // Relative assets directory
   },
 });
